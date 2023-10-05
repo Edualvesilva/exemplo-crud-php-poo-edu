da49dba145ab1b290244c52b5a6141d2ea063ac8
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-10-05 09:20:58 -0300

    método lerUmFabricante

--- src/Fabricante.php
+++ src/Fabricante.php
@@ -1,69 +1,95 @@
 <?php
+
 namespace ExemploCrudPoo;
+
 use Exception, PDO;
 
-final class Fabricante {
+final class Fabricante
+{
 
     private int $id;
     private string $nome;
-    
+
     /* Esta propriedade receberá os recursos
     PDO através da classe Banco (dependência deste projeto) */
     private PDO $conexao;
 
-    public function __construct() {
+    public function __construct()
+    {
         /* No momento em que um objeto Fabricante for
         criado, automaticamente será feita a chamada
         do método "conecta" existente na classe Banco. */
         $this->conexao = Banco::conecta();
     }
 
-    public function lerFabricantes():array {
+    public function lerFabricantes(): array
+    {
         $sql = "SELECT * FROM fabricantes ORDER BY nome";
-        
+
         try {
             $consulta = $this->conexao->prepare($sql);
             $consulta->execute();
             $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
         } catch (Exception $erro) {
-            die("Erro: ".$erro->getMessage());
-        }    
-    
+            die("Erro: " . $erro->getMessage());
+        }
+
         return $resultado;
-    } 
+    }
 
 
-    public function inserirFabricante():void {
+    public function inserirFabricante(): void
+    {
         $sql = "INSERT INTO fabricantes(nome) VALUES(:nome)";
-    
+
         try {
             $consulta = $this->conexao->prepare($sql);
             $consulta->bindValue(":nome", $this->nome, PDO::PARAM_STR);
             $consulta->execute();
         } catch (Exception $erro) {
-            die("Erro ao inserir: ".$erro->getMessage());
+            die("Erro ao inserir: " . $erro->getMessage());
         }
-    
     }
-    
 
 
-    public function getId(): int {
+    public function lerUmFabricante(): array
+    {
+        $sql = "SELECT * FROM fabricantes WHERE id = :id";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            $consulta->execute();
+            $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+        } catch (Exception $erro) {
+            die("Erro ao carregar: " . $erro->getMessage());
+        }
+
+        return $resultado;
+    }
+
+
+
+
+    public function getId(): int
+    {
         return $this->id;
     }
 
-    public function setId(int $id): self {
+    public function setId(int $id): self
+    {
         $this->id = $id;
         return $this;
     }
 
-    public function getNome(): string {
+    public function getNome(): string
+    {
         return $this->nome;
     }
 
-    public function setNome(string $nome): self {
-        $this->nome = filter_var($nome,FILTER_SANITIZE_SPECIAL_CHARS);
+    public function setNome(string $nome): self
+    {
+        $this->nome = filter_var($nome, FILTER_SANITIZE_SPECIAL_CHARS);
         return $this;
-
     }
 }
